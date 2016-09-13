080e3b04cdbcc39eae72891e13f6abec31e2e6eb
Bosko Ivanisevic <bosko.ivanisevic@gmail.com>
2016-09-13 20:25:30 +0200

    Replace obsolete function with new one

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -460,7 +460,7 @@
     (global-set-key (kbd "C-x C-f") 'counsel-find-file)
     (global-set-key (kbd "M-x") 'counsel-M-x)
     (global-set-key (kbd "<f6>") 'ivy-resume))
-  :bind (("C-c f" . ivy-recentf)
+  :bind (("C-c f" . counsel-recentf)
          ("C-c h i" . counsel-imenu)))
 
 (use-package company
