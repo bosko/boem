337cdf90527c8bdeb179ee1d16177a061aa3fe70
Bosko Ivanisevic <bosko.ivanisevic@gmail.com>
2016-09-28 11:23:26 +0200

    Change ivy-resume key binding

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -460,7 +460,7 @@
     (global-set-key (kbd "C-c c u") 'counsel-unicode-char)
     (global-set-key (kbd "C-x C-f") 'counsel-find-file)
     (global-set-key (kbd "M-x") 'counsel-M-x)
-    (global-set-key (kbd "C-c C-r") 'ivy-resume))
+    (global-set-key (kbd "C-c c r") 'ivy-resume))
   :bind (("C-c f" . counsel-recentf)
          ("C-c h i" . counsel-imenu)))
 
