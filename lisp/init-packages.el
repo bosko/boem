76dc5f4a49a2aa64f045547555e73a973c4fd4ac
Boško Ivanišević <bosko.ivanisevic@gmail.com>
2018-10-18 10:07:19 +0200

    Typescript - use tide package

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -710,6 +710,35 @@
     ;; (unbind-key "\C-m" coffee-mode-map)
     ))
 
+;;;; typescript
+(use-package tide
+  :ensure t
+  :config
+  (progn
+    (setq
+     tide-format-options '(:tabSize 2 :indentSize 2)
+     typescript-indent-level (or (plist-get (tide-tsfmt-options) ':indentSize) 2))
+
+    (defun setup-tide-mode ()
+      (interactive)
+      (tide-setup)
+      (flycheck-mode +1)
+      (setq flycheck-check-syntax-automatically '(save mode-enabled))
+      (eldoc-mode +1)
+      (tide-hl-identifier-mode +1)
+      ;; company is an optional dependency. You have to
+      ;; install it separately via package-install
+      ;; `M-x package-install [ret] company`
+      (company-mode +1))
+
+    ;; aligns annotation to the right hand side
+    (setq company-tooltip-align-annotations t)
+
+    ;; formats the buffer before saving
+    (add-hook 'before-save-hook 'tide-format-before-save)
+
+    (add-hook 'typescript-mode-hook #'setup-tide-mode)))
+
 ;;;; HTML
 
 ;;;; web-mode
