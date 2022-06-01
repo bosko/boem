554bd4e35fcc00f0292681ef370fbd52613a1a94
Boško Ivanišević <bosko.ivanisevic@gmail.com>
2022-06-01 16:58:40 +0200

    feat: disable Docker files editing - does not work
    There is an error on startup:
    
    Symbol's value as variable is void: tramp-methods

--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -207,26 +207,26 @@ Code from: http://emacsredux.com/blog/2013/04/28/switch-to-previous-buffer/"
 ;;; Edit file in Docker container.
 ;;; This was "stolen" from:
 ;;; https://willschenk.com/articles/2020/tramp_tricks/
-(push
- (cons
-  "docker"
-  '((tramp-login-program "docker")
-    (tramp-login-args (("exec" "-it") ("%h") ("/bin/bash")))
-    (tramp-remote-shell "/bin/sh")
-    (tramp-remote-shell-args ("-i") ("-c"))))
- tramp-methods)
+;; (push
+;;  (cons
+;;   "docker"
+;;   '((tramp-login-program "docker")
+;;     (tramp-login-args (("exec" "-it") ("%h") ("/bin/bash")))
+;;     (tramp-remote-shell "/bin/sh")
+;;     (tramp-remote-shell-args ("-i") ("-c"))))
+;;  tramp-methods)
 
-(defadvice tramp-completion-handle-file-name-all-completions
-    (around dotemacs-completion-docker activate)
-  "(tramp-completion-handle-file-name-all-completions \"\" \"/docker:\" returns
-    a list of active Docker container names, followed by colons."
-  (if (equal (ad-get-arg 1) "/docker:")
-      (let* ((dockernames-raw (shell-command-to-string "docker ps | awk '$NF != \"NAMES\" { print $NF \":\" }'"))
-             (dockernames (cl-remove-if-not
-                           #'(lambda (dockerline) (string-match ":$" dockerline))
-                           (split-string dockernames-raw "\n"))))
-        (setq ad-return-value dockernames))
-    ad-do-it))
+;; (defadvice tramp-completion-handle-file-name-all-completions
+;;     (around dotemacs-completion-docker activate)
+;;   "(tramp-completion-handle-file-name-all-completions \"\" \"/docker:\" returns
+;;     a list of active Docker container names, followed by colons."
+;;   (if (equal (ad-get-arg 1) "/docker:")
+;;       (let* ((dockernames-raw (shell-command-to-string "docker ps | awk '$NF != \"NAMES\" { print $NF \":\" }'"))
+;;              (dockernames (cl-remove-if-not
+;;                            #'(lambda (dockerline) (string-match ":$" dockerline))
+;;                            (split-string dockernames-raw "\n"))))
+;;         (setq ad-return-value dockernames))
+;;     ad-do-it))
 
 (provide 'init-basic)
 
