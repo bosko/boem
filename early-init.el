9813b23ba0c7df7ae4b899b98c133cc963752936
Boško Ivanišević <bosko.ivanisevic@gmail.com>
2026-07-06 10:21:09 +0200

    Speed up starting, early turn off menu, toolbar, etc.

--- early-init.el
+++ early-init.el
@@ -3,3 +3,31 @@
   (startup-redirect-eln-cache
    (expand-file-name
     "eln-cache" (expand-file-name "data" user-emacs-directory))))
+
+;; Delay garbage collection while Emacs is booting
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6)
+
+;; Schedule garbage collection sensible defaults for after booting
+(add-hook 'after-init-hook
+          (lambda ()
+            (setq gc-cons-threshold (* 100 1024 1024)
+                  gc-cons-percentage 0.1)))
+
+;; Single VC backend inscreases booting speed
+(setq vc-handled-backends '(Git))
+
+;; Better Window Management handling
+(setq frame-resize-pixelwise t
+      frame-inhibit-implied-resize t)
+
+;; Disables unused UI Elements
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'tooltip-mode) (tooltip-mode -1))
+
+;; Avoid raising the *Messages* buffer if anything is still without
+;; lexical bindings
+(setq warning-minimum-level :error)
+(setq warning-suppress-types '((lexical-binding)))
--- init.el
+++ init.el
@@ -3,10 +3,6 @@
 ;;; Commentary:
 ;;
 
-;; Make startup faster by reducing the frequency of garbage
-;; collection.  The default is 800 kilobytes. Measured in bytes.
-(setq gc-cons-threshold (* 50 1000 1000))
-
 (defvar boem-init-root
   (expand-file-name (file-name-directory load-file-name)))
 
