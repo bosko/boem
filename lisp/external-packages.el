ddc13769869c0f8dc7b290f3dcc69413a6c818a2
Boško Ivanišević <bosko.ivanisevic@gmail.com>
2026-07-08 11:13:51 +0200

    Setting commentary and lexical-binding

--- lisp/external-packages.el
+++ lisp/external-packages.el
@@ -1,1 +1,15 @@
+;;; internal-packages.el --- Internal Emacs packages -*- lexical-binding: t; -*-
+;;
+;; Author: Boško Ivanišević
+;; URL: https://github.com/bosko/boem
+;; Package-Requires: ((emacs "30.1"))
+;; Keywords: config
+;; SPDX-License-Identifier: GPL-3.0-or-later
+;;
+
+;;; Commentary:
+;;  Enabling and configuring internal packages
+;;
+
+;;; Code:
 (provide 'external-packages)
--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -1,10 +1,17 @@
-;;; init-basic.el --- Basic configurations
+;;; init-basic.el --- Basic configurations -*- lexical-binding: t; -*-
+;;
+;; Author: Boško Ivanišević
+;; URL: https://github.com/bosko/boem
+;; Package-Requires: ((emacs "30.1"))
+;; Keywords: config
+;; SPDX-License-Identifier: GPL-3.0-or-later
+;;
 
 ;;; Commentary:
+;;  Basic Emacs Boem functions
 ;;
 
 ;;; Code:
-
 (defun boem/data-path (key)
   "Return the absolute path for KEY in `boem-data-paths'."
   (let ((rel (cdr (assq key boem-data-paths))))
